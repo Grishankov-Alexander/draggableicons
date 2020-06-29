087ff28ece0eca1c80f7996b58eb8560b8911d88
Grishankov Alexander <grishankov.alexander@gmail.com>
2020-06-29 13:45:18 +0300

    Add sources.

---
+++ Qt-draggableicons.pro
@@ -0,0 +1,11 @@
+QT += widgets
+
+RESOURCES += \
+    draggableicons.qrc
+
+HEADERS += \
+    dragwidget.h
+
+SOURCES += \
+    dragwidget.cpp \
+    main.cpp
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <QApplication>
+#include <QHBoxLayout>
+
+#include "dragwidget.h"
+
+int main(int argc, char **argv)
+{
+    Q_INIT_RESOURCE(draggableicons);
+    QApplication app(argc, argv);
+    QWidget mainWidget;
+    QHBoxLayout *horizontalLayout = new QHBoxLayout(&mainWidget);
+    horizontalLayout->addWidget(new DragWidget);
+    horizontalLayout->addWidget(new DragWidget);
+    mainWidget.setWindowTitle(QObject::tr("Draggable Icons"));
+    mainWidget.show();
+    return app.exec();
+}
